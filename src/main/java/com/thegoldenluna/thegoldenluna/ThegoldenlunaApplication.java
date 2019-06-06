ff9b43ae0a9bc58641489053a50caeaa0612b3a2
Travis Vela <jtravisvela@gmail.com>
2019-06-05 19:30:26 -0500

    first commit

---
+++ src/main/java/com/thegoldenluna/thegoldenluna/ThegoldenlunaApplication.java
@@ -0,0 +1,13 @@
+package com.thegoldenluna.thegoldenluna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegoldenlunaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThegoldenlunaApplication.class, args);
+    }
+
+}
