c10a75513ca14c662deb9bc2576845e36d00c5e7
Travis Vela <jtravisvela@gmail.com>
2019-06-11 20:36:42 -0500

    add link to profile page from the popover/link text. implement on both index page and single post page

--- src/main/java/com/thegoldenluna/thegoldenluna/controllers/ProfileController.java
+++ src/main/java/com/thegoldenluna/thegoldenluna/controllers/ProfileController.java
@@ -7,10 +7,7 @@ import com.thegoldenluna.thegoldenluna.repositories.UserRepo;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +23,7 @@ public class ProfileController {
         this.postRepo = postRepo;
     }
 
+    // this is the logged in users profile
     @GetMapping("/profile")
     public String profile(Model model) {
         User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -48,5 +46,24 @@ public class ProfileController {
     @ResponseBody
     public Post findOne(long id) {
         return postRepo.findOne(id);
+    }
+
+    // this is the generic user profile
+    @GetMapping("/profile/{id}")
+    public String thisProfile(@PathVariable long id, Model model) {
+
+        User thisUser = userRepo.findOne(id);
+
+        List<Post> userPosts = new ArrayList<>();
+
+        for (Post post : postRepo.findAll()) {
+            if (post.getUser().getId() == thisUser.getId()) {
+                userPosts.add(post);
+            }
+        }
+
+        model.addAttribute("posts", userPosts);
+        model.addAttribute("user", thisUser);
+        return "users/user-profile";
     }
 }
