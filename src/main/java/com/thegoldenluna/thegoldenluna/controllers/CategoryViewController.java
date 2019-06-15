1dcac3b84663dba38d7e06453fc0d079abdf5bb2
Travis Vela <jtravisvela@gmail.com>
2019-06-14 19:04:44 -0500

    add other categories carousel to the bottom of category view page linkin to all other cats

--- src/main/java/com/thegoldenluna/thegoldenluna/controllers/CategoryViewController.java
+++ src/main/java/com/thegoldenluna/thegoldenluna/controllers/CategoryViewController.java
@@ -2,8 +2,11 @@ package com.thegoldenluna.thegoldenluna.controllers;
 
 import com.thegoldenluna.thegoldenluna.models.Category;
 import com.thegoldenluna.thegoldenluna.models.Post;
+import com.thegoldenluna.thegoldenluna.models.User;
 import com.thegoldenluna.thegoldenluna.repositories.CategoryRepo;
 import com.thegoldenluna.thegoldenluna.repositories.PostRepo;
+import com.thegoldenluna.thegoldenluna.repositories.UserRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +17,21 @@ import java.util.List;
 
 @Controller
 public class CategoryViewController {
+    private UserRepo userRepo;
     private PostRepo postRepo;
     private CategoryRepo categoryRepo;
 
-    public CategoryViewController(PostRepo postRepo, CategoryRepo categoryRepo) {
+    public CategoryViewController(PostRepo postRepo, CategoryRepo categoryRepo, UserRepo userRepo) {
         this.postRepo = postRepo;
         this.categoryRepo = categoryRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping("/posts/category/{id}")
     public String categoryPosts(@PathVariable long id,  Model model) {
+        User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User DbUser = userRepo.findOne(sessionUser.getId());
+
         Iterable<Post> posts = postRepo.findAll();
         Category category = categoryRepo.findOne(id);
         List<Post> categoryPosts = new ArrayList<>();
@@ -33,7 +41,16 @@ public class CategoryViewController {
                 categoryPosts.add(post);
             }
         }
+
+        List<Category> otherCats = new ArrayList<>();
+        for (Category cat : categoryRepo.findAll()) {
+            if (cat.getId() != category.getId()) {
+                otherCats.add(cat);
+            }
+        }
         model.addAttribute("posts", categoryPosts);
+        model.addAttribute("user", DbUser);
+        model.addAttribute("otherCats", otherCats);
         return "/posts/category";
     }
 
