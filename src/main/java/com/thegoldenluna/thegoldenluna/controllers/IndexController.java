843e8fe48333c1a3dc5790b255260478297b744d
Travis Vela <jtravisvela@gmail.com>
2019-06-15 08:49:14 -0500

    add card tabs for all and category to index page

--- src/main/java/com/thegoldenluna/thegoldenluna/controllers/IndexController.java
+++ src/main/java/com/thegoldenluna/thegoldenluna/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.thegoldenluna.thegoldenluna.controllers;
 
+import com.thegoldenluna.thegoldenluna.repositories.CategoryRepo;
 import com.thegoldenluna.thegoldenluna.repositories.PostRepo;
 import com.thegoldenluna.thegoldenluna.repositories.UserRepo;
 import org.springframework.stereotype.Controller;
@@ -10,18 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     private UserRepo userRepo;
     private PostRepo postRepo;
+    private CategoryRepo categoryRepo;
 
-    public IndexController(UserRepo userRepo, PostRepo postRepo) {
+    public IndexController(UserRepo userRepo, PostRepo postRepo, CategoryRepo categoryRepo) {
         this.userRepo = userRepo;
         this.postRepo = postRepo;
+        this.categoryRepo = categoryRepo;
     }
 
     @GetMapping("/home")
     public String index(Model model) {
 
-
-
         model.addAttribute("posts", postRepo.findAll());
+        model.addAttribute("categories", categoryRepo.findAll());
         return "index";
     }
 }
